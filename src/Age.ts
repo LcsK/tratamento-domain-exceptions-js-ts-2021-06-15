fbd7b4215ae31012058de3795944bd59b77c210e
lflopes <lflopes@ibm.com>
2021-06-17 20:22:18 -0300

    passo 6: implementação do InvalidAge
    Adicionamos mais essa camada de interface para garantir uma maior clareza
    no que está sendo retornado pelo método

--- src/Age.ts
+++ src/Age.ts
@@ -1,11 +1,15 @@
 import { DomainError } from "./DomainError";
 
-const createInvalidAge = (ageValue: number): DomainError => ({
+export interface InvalidAge extends DomainError {
+	name: 'InvalidAge';
+}
+
+const createInvalidAge = (ageValue: number): InvalidAge => ({
 	name: 'InvalidAge',
 	message: `Idade deve ser um número positivo e inteiro. Recebeu: ${ageValue}`,
 });
 
-export const createAge = (ageValue: number): number | DomainError => {
+export const createAge = (ageValue: number): number | InvalidAge => {
 	if (ageValue < 0 || !Number.isInteger(ageValue)) {
 		return createInvalidAge(ageValue);
 	}
--- src/Person.ts
+++ src/Person.ts
@@ -1,5 +1,4 @@
-import { createAge } from "./Age";
-import { DomainError } from "./DomainError";
+import { createAge, InvalidAge } from "./Age";
 import { createNickname } from "./Nickname";
 
 export interface PersonValues {
@@ -12,11 +11,11 @@ export interface Person {
 	nickname: string;
 }
 
-export const createPerson = (personValues: PersonValues): Person | DomainError => {
+export const createPerson = (personValues: PersonValues): Person | InvalidAge => {
 	const { age: ageValue, nickname: nicknameValue } = personValues;
 
 	const age = createAge(ageValue);
-	if (typeof age !== 'number' && (age as DomainError).name === 'InvalidAge') {
+	if (typeof age !== 'number' && (age as InvalidAge).name === 'InvalidAge') {
 		return age;
 	}
 
