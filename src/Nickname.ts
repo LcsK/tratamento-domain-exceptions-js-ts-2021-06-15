69cdfdd9df39a9ab3fae6e0fd227c5906ec8c9f0
lflopes <lflopes@ibm.com>
2021-06-17 21:34:53 -0300

    passo 12: implementa validações em Nickname
    As validações em Nickname seguem o mesmo padrão de Age

--- src/Nickname.ts
+++ src/Nickname.ts
@@ -1,3 +1,22 @@
-export const createNickname = (nicknameValue: string): string => {
-	return nicknameValue;
+import { Either, createFailureResult, createSuccessResult } from "./DomainResult";
+import { DomainError } from "./DomainError";
+
+export interface EmptyNickname extends DomainError {
+	name: 'EmptyNickname';
+}
+
+const createEmptyNickname = (): EmptyNickname => ({
+	name: 'EmptyNickname',
+	message: `Nickname should not be an empty string`,
+});
+
+export const createNickname = (nicknameValue: string): Either<string, EmptyNickname> => {
+	if (!nicknameValue) {
+		return createFailureResult<string, EmptyNickname>(
+			createEmptyNickname(),
+		);
+	}
+	return createSuccessResult<string, EmptyNickname>(
+		nicknameValue,
+	);
 }
