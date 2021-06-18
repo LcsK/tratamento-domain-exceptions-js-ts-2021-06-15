3fb48520974944a5e4cc8fe4d02a2414e3f8205b
lflopes <lflopes@ibm.com>
2021-06-17 21:47:15 -0300

    passo 14: adiciona cenários de teste
    Com estes cenários é possível visualizar cada uma das possibilidades
    de estados das entidades da aplicação

--- app.ts
+++ app.ts
@@ -1,9 +1,29 @@
 import { createPerson } from "./src/Person";
 
 
-const person = createPerson({
+const wrongPerson = createPerson({
+	age: -1.4,
+	nickname: '',
+});
+const negativeAgePerson = createPerson({
 	age: -1,
-	nickname: 'lucas',
+	nickname: 'lcsk',
+});
+const nonIntegerAgePerson = createPerson({
+	age: 1.4,
+	nickname: 'lcsk',
+});
+const emptyNicknamePerson = createPerson({
+	age: 1,
+	nickname: '',
+});
+const person = createPerson({
+	age: 1,
+	nickname: 'lcsk',
 });
 
-console.log(person);
+console.log(`----\nWrong Person:\n${JSON.stringify(wrongPerson.value, null, 1)}\n----\n`);
+console.log(`----\nNegativeAge Person:\n${JSON.stringify(negativeAgePerson.value, null, 1)}\n----\n`);
+console.log(`----\nNonIntegerAge Person:\n${JSON.stringify(nonIntegerAgePerson.value, null, 1)}\n----\n`);
+console.log(`----\nEmptyNickname Person:\n${JSON.stringify(emptyNicknamePerson.value, null, 1)}\n----\n`);
+console.log(`----\nPerson:\n${JSON.stringify(person.value, null, 1)}\n----\n`);
