ee65b13ac7ad113841a836e9128ded3b077fd3f1
lflopes <lflopes@ibm.com>
2021-06-17 21:14:46 -0300

    passo 10: usando InvalidAge
    InvalidAge nos ajuda a termos uma granularidade de quais são os
    possíveis erros em Age, sem termos que executar o método duas vezes
    para o mesmo valor

--- src/Age.ts
+++ src/Age.ts
@@ -1,12 +1,19 @@
 import { Either, createFailureResult, createSuccessResult } from "./DomainResult";
 import { DomainError } from "./DomainError";
 
+type InvalidAgeStates = NegativeAge | NonIntegerAge;
+
 export interface NegativeAge extends DomainError {
 	name: 'NegativeAge';
 }
 
 export interface NonIntegerAge extends DomainError {
 	name: 'NonIntegerAge';
+}
+
+export interface InvalidAge extends DomainError {
+	name: 'InvalidAge';
+	invalidStates: InvalidAgeStates[];
 }
 
 const createNegativeAge = (ageValue: number): NegativeAge => ({
@@ -19,16 +26,31 @@ const createNonIntegerAge = (ageValue: number): NonIntegerAge => ({
 	message: `Idade deve ser um número inteiro. Recebeu: ${ageValue}`,
 });
 
-export const createAge = (ageValue: number): Either<number, NegativeAge | NonIntegerAge> => {
+const createInvalidAge = (invalidAgeStates: InvalidAgeStates[]): InvalidAge => ({
+	name: 'InvalidAge',
+	invalidStates: invalidAgeStates,
+	message: 'Idade inválida',
+});
+
+export const createAge = (ageValue: number): Either<number, InvalidAge> => {
+	const invalidAgeStates: InvalidAgeStates[] = [];
 	if (ageValue < 0) {
-		return createFailureResult<number, NegativeAge>(
+		invalidAgeStates.push(
 			createNegativeAge(ageValue)
 		);
-	} else if (!Number.isInteger(ageValue)) {
-		return createFailureResult<number, NonIntegerAge>(
+	}
+	// ATENÇÃO: Na aula eu esqueci de refatorar de "else if" para somente "if"
+	if (!Number.isInteger(ageValue)) {
+		invalidAgeStates.push(
 			createNonIntegerAge(ageValue)
 		);
 	}
 
-	return createSuccessResult<number, NegativeAge | NonIntegerAge>(ageValue);
+	if (invalidAgeStates.length) {
+		return createFailureResult<number, InvalidAge>(
+			createInvalidAge(invalidAgeStates),
+		);
+	}
+
+	return createSuccessResult<number, InvalidAge>(ageValue);
 }
